8be3775084a650c401defac7b24a0a752087cc28
hochan <hochan9@gmail.com>
2025-06-19 10:28:37 +0900

    feat: init commit

---
+++ feign/lib/src/main/java/com/sample/client/WalletClient.java
@@ -0,0 +1,27 @@
+/*
+ * Created by Hochan Son on 2025. 6. 19.
+ * As part of
+ *
+ * Copyright (C)  () - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Backend Team <hc.son9@google.com>, 2025. 6. 19.
+ */
+
+package com.sample.client;
+
+/**
+ * create on 2025. 6. 19. create by IntelliJ IDEA.
+ * create by IntelliJ IDEA.
+ *
+ * <p> 클래스 설명. </p>
+ * <p> {@link } and {@link }관련 클래스 </p>
+ *
+ * @author Hochan Son
+ * @version 1.0
+ * @see
+ * @since 지원하는 자바버전 (ex : 5+ 5이상)
+ */
+public interface WalletClient {
+
+}
